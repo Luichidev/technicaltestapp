1643c1277cee09a2ec5ad1818c3eb4834c9bdff4
@LuisArana <disconluis@gmail.com>
2022-04-08 00:21:18 +0200

    feat: Add services fetch

---
+++ src/services/getUserComments.js
@@ -0,0 +1,22 @@
+export default function getUserCommnets() {
+  const apiUrl = 'http://localhost:3000/'
+  return fetch(apiUrl)
+    .then((res) => res.json())
+    .then((response) => {
+      if (Array.isArray(response)) {
+        const comments = response.map((comment) => {
+          const { firstName, lastName, picture } = comment.owner
+          const { text, comments, id } = comment
+          return {
+            text,
+            lastName,
+            firstName,
+            picture,
+            comments,
+            id,
+          }
+        })
+        return comments
+      }
+    })
+}
